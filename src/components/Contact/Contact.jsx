c99035540ab60496297b3a87506d0979d40245cf
mahadi <sagor.gsc15@gmail.com>
2023-06-14 01:14:16 +0600

    reset done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import emailjs, { send } from '@emailjs/browser';
+// import { useForm } from "react-hook-form";
 
 const Contact = () => {
 
-    // email starts here
+    const [form_name, setFormName] = useState("");
+    const [form_email, setFormEmail] = useState("");
+    const [form_message, setFormMessage] = useState("");
+    // const [click, setClick] = useState(false);
 
     const form = useRef();
 
@@ -17,7 +21,25 @@ const Contact = () => {
             }, (error) => {
                 console.log(error.text);
             });
+
+        setFormName("");
+        setFormEmail("");
+        setFormMessage("");
+
     };
+
+    // const handleSubmitAndReset = () => {      
+
+    //     sendEmail();
+    //     setFormName("");
+    //     setFormEmail("");
+    //     setFormMessage("");
+
+    // }
+
+
+
+
 
     // email ends here
 
@@ -29,19 +51,21 @@ const Contact = () => {
                 <form ref={form} onSubmit={sendEmail}>
 
                     <h4 className='mt-4'>Username</h4>
-                    <input className='py-3 w-75' type="text" name='form_name' placeholder="Enter your username" />
+
+                    {/* onChange={(e) => setName(e.target.value)} */}
+                    <input className='py-3 w-75' type="text" name='form_name' value={form_name} onChange={(e) => setFormName(e.target.value)} placeholder="Enter your username" />
 
 
                     <h4 className='mt-4'>Email</h4>
-                    <input className='py-3 w-75' type="text" name='gorm_email' placeholder="Enter your Email" />
+                    <input className='py-3 w-75' type="text" name='form_email' value={form_email} onChange={(e) => setFormEmail(e.target.value)} placeholder="Enter your Email" />
 
 
 
                     <h4 className='mt-4'>Write any suggestion here</h4>
-                    <textarea className='w-75' name='form_message' rows="5"></textarea>
+                    <textarea className='w-75' name='form_message' value={form_message} onChange={(e) => setFormMessage(e.target.value)} rows="5"></textarea>
 
                     <div>
-                        <button className='btn btn-primary mx-auto' type='submit' value={send}>Send</button>
+                        <button className='btn btn-primary mx-auto' type='submit'>Send</button>
                     </div>
 
                 </form>
